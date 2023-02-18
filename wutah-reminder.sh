ef0abbf981933dc09905aca23c091216f3e2f1ef
thuan.ngo <thuan.ngo@eastagile.com>
2023-02-18 15:03:37 +0700

    :tada: Fix typo

--- wutah-reminder.sh
+++ wutah-reminder.sh
@@ -1,4 +1,4 @@
-/usr/local/bin/terminal-notifier -message "Don't forget to hydrae, champ!"\
+/usr/local/bin/terminal-notifier -message "Don't forget to hydrate, champ!"\
                   -title "Water checkpoint 💧"\
                   -sound Crystal\
                   -ignoreDnD
